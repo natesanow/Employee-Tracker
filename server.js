0d109126c0802d5e5584be824011280d7355fbd2
natesanow <152323081+natesanow@users.noreply.github.com>
2024-03-11 11:58:06 -0700

    added question prompts

--- server.js
+++ server.js
@@ -1,3 +1,41 @@
 const express = require('express');
 const mysql = require('mysql12');
 const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '57Oliveway!',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+ function questions() {
+    inquirer
+    .prompt([{
+        type: "list",
+        name: "starterQuestion",
+        message: "What would you like to do?",
+        choices: [
+            "View all Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View all Roles",
+            "Add Role",
+            "View all Departments",
+            "Add Department",
+        ]
+    }])
+ }
